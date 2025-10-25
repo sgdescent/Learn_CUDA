adf6f7547b51a1e993345f71fd0998220b57c73e
Saksham Gupta <sakshamg@catalyst-0-9.eth>
2025-10-25 05:12:33 -0400

    Fixes Some simple errors

--- Matrix Operations/Matrix_Vector_Mul.cu
+++ Matrix Operations/Matrix_Vector_Mul.cu
@@ -1,13 +1,9 @@
-"""
-Given a matrix A and a vector b, compute the product Ab.
-"""
+//Given a matrix A and a vector b, compute the product Ab.
 #include<iostream>
 #include<cuda_runtime.h>
 #include<chrono>
 
-#define cudaErrorCheck(ans){
-    gpuAssert((ans),__FILE__,__LINE__);
-}
+#define cudaErrorCheck(ans){gpuAssert((ans),__FILE__,__LINE__);}
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true){
     if (code!=cudaSuccess){
         fprintf(stderr,"GPUAssert: %s %s %d\n", cudaGetErrorString(code),file,line);
@@ -23,7 +19,7 @@ __global__ void matrix_vector_multiplication_kernel(const float *A, const float 
         int row  = blockIdx.y*blockDim.y + threadIdx.y;
         float cval = 0;
         if (row < M){
-        for(int i=0;i<N;i++){
+        for(int i=0;i<N;++i){
             cval += A[row*N+i]*B[i]; 
         }
     }
@@ -45,7 +41,7 @@ int main(){
     printf("Performing Matrix multiplication Matrix A (%d,%d) with Vector B (%d,)",M,N,N);
 
     //Setup the Data on the host (CPU)
-    size_t size_A = (size_t)M*N*sizeof(float):
+    size_t size_A = (size_t)M*N*sizeof(float);
     size_t size_B = (size_t)N*sizeof(float);
     size_t size_C = (size_t)M*sizeof(float);
 
@@ -55,12 +51,23 @@ int main(){
     float *h_B = new float[N];
     float *h_C = new float[M];
 
-    for(int i=0; i<M*N; ++i){
-        h_A[i] = (float)rand() / RAND_MAX;
+    // Initialize and print Matrix A
+    printf("\nMatrix A:\n");
+    for(int i=0; i<M; ++i){
+        for(int j=0; j<N; ++j){
+            h_A[i*N + j] = (float)rand() / RAND_MAX;
+            printf("%.3f\t", h_A[i*N + j]);
+        }
+        printf("\n");
     }
+
+    // Initialize and print Vector B
+    printf("\nVector B:\n");
     for(int i=0; i<N; ++i){
         h_B[i] = (float)rand() / RAND_MAX;
+        printf("%.3f\n", h_B[i]);
     }
+    printf("\n");
 
     //Allocate memory on device
     float *d_A, *d_B, *d_C;
@@ -78,9 +85,18 @@ int main(){
 
     cudaErrorCheck(cudaMemcpy(h_C, d_C, size_C, cudaMemcpyDeviceToHost));
 
-    for(int i =0; i<M, ++i){
-        printf("%d",h_C[i])
+    for(int i =0; i<M; ++i){
+        printf("%f\t", h_C[i]);
     }
+
+    //Free memory
+    delete[] h_A;
+    delete[] h_B;
+    delete[] h_C;
+
+    cudaErrorCheck(cudaFree(d_A));
+    cudaErrorCheck(cudaFree(d_B));
+    cudaErrorCheck(cudaFree(d_C));
 
 
 }
